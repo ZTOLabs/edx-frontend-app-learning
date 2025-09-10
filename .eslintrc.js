f589ac2542b3c2ae798d04aadbe22a185e53e904
Hieu Nguyen <thanhhieunguyen183@gmail.com>
2025-09-10 11:45:59 +0700

    feat: Setup project

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,8 @@ const config = createConfig('eslint', {
     'react/jsx-no-useless-fragment': 'off',
     'react/no-unknown-property': 'off',
     'func-names': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
   },
   settings: {
     'import/resolver': {
