3405861c5316400f0e1ffdc43fc82a337fe7ffd1
Hieu Nguyen <thanhhieunguyen183@gmail.com>
2025-09-17 11:10:24 +0700

    feat: Update nav item hovering effect

--- src/shared/components/Common/Layouts/AppSidebar/Navigation/item.tsx
+++ src/shared/components/Common/Layouts/AppSidebar/Navigation/item.tsx
@@ -32,7 +32,7 @@ const Items = ({
             }
           }}
         >
-          <div className={`tw-w-12 tw-h-12 tw-flex tw-items-center tw-justify-center tw-rounded-[12px] ${item.isActive ? 'tw-bg-brand-100' : ''}`}>
+          <div className={`tw-w-12 tw-h-12 tw-flex tw-items-center tw-justify-center tw-rounded-[12px] hover:tw-bg-brand-100 ${item.isActive ? 'tw-bg-brand-100' : ''}`}>
             {item.isActive && item.activeIcon ? <item.activeIcon /> : <item.icon className="tw-text-gray-500 tw-size-5" />}
           </div>
           {item.title && <div className="tw-text-gray-700 tw-text-xs tw-font-normal tw-h-[18px]">{item.title}</div>}
