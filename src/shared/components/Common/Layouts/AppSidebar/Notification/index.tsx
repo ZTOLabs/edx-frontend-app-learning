0ab91137c10fbb04190fee40f890fb8bf8601897
Hieu Nguyen <thanhhieunguyen183@gmail.com>
2025-09-19 13:19:26 +0700

    feat: Update notification effect

--- src/shared/components/Common/Layouts/AppSidebar/Notification/index.tsx
+++ src/shared/components/Common/Layouts/AppSidebar/Notification/index.tsx
@@ -1,32 +1,37 @@
 import { Bell01 } from '@untitledui/icons';
-import React from 'react';
+import React, { useState } from 'react';
+import Bell01SolidIcon from 'shared/icons/Bell01SolidIcon';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '../../../../ui/dropdown-menu';
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '../../../../ui/sidebar';
 
-const Notification = () => (
-  <SidebarMenu className="!tw-w-auto tw-list-none !tw-p-0">
-    <SidebarMenuItem className="tw-flex tw-items-center tw-justify-center">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <SidebarMenuButton
-            size="lg"
-            className="!tw-bg-inherit !tw-p-0 tw-border-none tw-w-full tw-flex tw-items-center tw-justify-center"
+const Notification = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <SidebarMenu className="!tw-w-auto tw-list-none !tw-p-0">
+      <SidebarMenuItem className={`tw-flex tw-items-center tw-justify-center tw-overflow-hidden tw-rounded-xl ${isOpen ? 'tw-bg-brand-100' : 'hover:tw-bg-brand-100'}`}>
+        <DropdownMenu onOpenChange={setIsOpen}>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="!tw-bg-inherit !tw-p-0 tw-border-none tw-w-full tw-flex tw-items-center tw-justify-center"
+            >
+              {isOpen ? <Bell01SolidIcon className="!tw-size-5 tw-text-brand-600" /> : <Bell01 className="!tw-size-5 tw-text-gray-500" />}
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="tw-w-(--radix-dropdown-menu-trigger-width) tw-min-w-56 tw-rounded-lg tw-bg-white"
+            side="right"
+            align="end"
+            sideOffset={4}
           >
-            <Bell01 className="!tw-size-5 tw-text-gray-500" />
-          </SidebarMenuButton>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent
-          className="tw-w-(--radix-dropdown-menu-trigger-width) tw-min-w-56 tw-rounded-lg tw-bg-white"
-          side="right"
-          align="end"
-          sideOffset={4}
-        >
-          {/* TODO: Add Notification Items */}
-          Notification
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </SidebarMenuItem>
-  </SidebarMenu>
-);
+            {/* TODO: Add Notification Items */}
+            Notification
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+};
 
 export default Notification;
