0d4d768199e349ee08b10a6acb17e60797084c5e
ninh-nd <ninh.nd@outlook.com>
2025-09-22 16:48:45 +0700

    style: style fix

--- src/course-home/outline-tab/Tag.jsx
+++ src/course-home/outline-tab/Tag.jsx
@@ -4,7 +4,7 @@ const Tag = ({ tagName }) => {
   if (!tagName) return null;
 
   return (
-    <div className="tw-px-1.5 tw-py-0.5 tw-bg-white tw-rounded-[6px] tw-shadow-[0px_1px_2px_0px_rgba(16,24,40,0.05)] tw-outline tw-outline-1 tw-outline-offset-[-1px] tw-outline-gray-300 tw-inline-flex tw-justify-start tw-items-center tw-w-fit tw-h-[18px]">
+    <div className="tw-px-1.5 tw-py-0.5 tw-bg-white tw-rounded-[6px] tw-shadow-[0px_1px_2px_0px_rgba(16,24,40,0.05)] tw-outline tw-outline-1 tw-outline-offset-[-1px] tw-outline-gray-300 tw-inline-flex tw-justify-start tw-items-center tw-w-fit tw-h-[22px]">
       <div className="tw-text-center tw-justify-start tw-text-slate-700 tw-text-xs tw-font-medium tw-leading-none">
         {tagName}
       </div>
